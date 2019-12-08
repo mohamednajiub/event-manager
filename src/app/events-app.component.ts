9d9be10de813904f81abf1f7411b8227efc8a8d3
Mohamed Najiub <mohamednajiub@gmail.com>
2019-12-08 11:38:02 +0200

    feat: add bootstrap and remove some package

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app-root',
+  template: `<h2>Events Manager</h2>`
+})
+export class EventsAppComponent {
+  title = 'event-manager';
+}
