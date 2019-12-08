e18b4939c905be5f3b1c71353ed0b61874f2efea
Mohamed Najiub <mohamednajiub@gmail.com>
2019-12-08 13:22:22 +0200

    feat: add jquery

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app-root',
-  template: `<h2>Events Manager</h2>`
+  template: `<div class="container">
+  <div class="jumbotron">
+    <h1>Welcome</h1>
+    <h2>Angular & Bootstrap Demo</h2>
+  </div>
+  <div class="panel panel-primary">
+    <div class="panel-heading">Status</div>
+    <div class="panel-body">
+      <h3>{{title}}</h3>
+    </div>
+  </div>
+</div>`
 })
 export class EventsAppComponent {
-  title = 'event-manager';
+  title = 'Event Manager';
 }
